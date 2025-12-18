76755dd2b9e2b9a2baa42f62d1134909ec9c4591
noobass <mikolka1979@gmail.com>
2025-12-18 16:02:35 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -9,26 +9,22 @@
 #include <cstdint>
 #define isn isnatural
 
-bool isnatural(int32_t x) {
-    return x > 0 && x == static_cast<int32_t>(x);
+void is_natural(int32_t* x) {
+    while (!(std::cin >> *x) || *x <= 0) {
+        std::cout << "Please enter integer: ";
+    }
 }
 
-isint(int32_t x){
-    return x == static_cast<int32_t>(x);
-}
-
-void fail() {
-    std::cout << "Ќу как так вы совершили неправильный ввод подумайте над своим поведением" << "\n";
+void is_int(int32_t* x) {
+    while (!(std::cin >> *x) || *x != static_cast<int32_t>(*x)) {
+        std::cout << "Please enter integer: ";
+    }
 }
 
 void normal(int32_t** a, int32_t n, int32_t m) {
     for (int32_t i = 0; i < n; ++i) {
         for (int32_t j = 0; j < m; ++j) {
-            std::cin >> a[i][j];
-            if (!isint(a[i][j])) {
-                fail();
-                exit(0);
-            }
+            is_int(&a[i][j]);
         }
     }
 }
@@ -36,11 +32,9 @@ void normal(int32_t** a, int32_t n, int32_t m) {
 void random(int32_t** a, int32_t n, int32_t m) {
     std::cout << "Please enter the interval of numbers in array(min max): ";
     int32_t l, r;
-    std::cin >> l >> r;
-    if (!isint(l) || !isint(r)) {
-        fail();
-        exit(0);
-    }
+    is_int(&l);
+    is_int(&r);
+    srand(time(nullptr));
     std::cout << "Your matrix: \n";
     for (int32_t i = 0; i < n; ++i) {
         for (int32_t j = 0; j < m; ++j) {
@@ -51,7 +45,7 @@ void random(int32_t** a, int32_t n, int32_t m) {
     }
 }
 
-void solve(int32_t** a, int32_t n, int32_t m, int32_t* sum, int32_t* b) {
+void summ(int32_t** a, int32_t n, int32_t m, int32_t* sum, int32_t* b) {
     int32_t ans = 0;
     for (int32_t i = 0; i < n; ++i) {
         for (int32_t j = 0; j < m; ++j) {
@@ -69,46 +63,54 @@ void solve(int32_t** a, int32_t n, int32_t m, int32_t* sum, int32_t* b) {
     std::cout << "Sum of col with 0 is equal: " << ans << "\n";
 }
 
+void srt(int32_t** a, int32_t n, int32_t m) {
+    for (int32_t i = 0; i < n; ++i) {
+        if (i % 2 == 0) {
+            std::sort(a[i], a[i] + m);
+        }
+        else {
+            std::sort(a[i], a[i] + m);
+            std::reverse(a[i], a[i] + m);
+        }
+    }
+    std::cout << "Your sorted matrix: \n";
+    for (int32_t i = 0; i < n; ++i) {
+        for (int32_t j = 0; j < m; ++j) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << '\n';
+    }
+}
+
 int32_t main() {
     int32_t n, m;
     SetConsoleOutputCP(1251);
     std::cout << "Enter quantity of rows and columns: ";
-    std::cin >> n >> m;
+    is_int(&n);
+    is_int(&m);
 
-    if (!isn(n) || !isn(m)) {
-        fail();
-        return 0;
-    }
-
-    int32_t** a = new int32_t*[n];
+    int32_t** a = new int32_t * [n];
     for (int32_t i = 0; i < n; ++i) {
         a[i] = new int32_t[m];
     }
 
-    int32_t* sum = new int32_t[m];
-    int32_t* b = new int32_t[m];
-
-    for (int32_t i = 0; i < m; ++i) {
-        sum[i] = 0;
-        b[i] = 0;
-    }
+    int32_t* sum = new int32_t[m]{};
+    int32_t* b = new int32_t[m]{};
 
     std::cout << "Normal or Random input?!(N or R): ";
     char x;
-    std::cin >> x;
-
+    while (!(std::cin >> x) || (x != 'N' && x != 'R')) {
+        std::cout << "N or R: ";
+    }
     if (x == 'N') {
         normal(a, n, m);
     }
     else if (x == 'R') {
         random(a, n, m);
     }
-    else {
-        fail();
-        return 0;
-    }
 
-    solve(a, n, m, sum, b);
+    summ(a, n, m, sum, b);
+    srt(a, n, m);
 
     for (int32_t i = 0; i < n; ++i) {
         delete[] a[i];
